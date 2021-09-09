9f8db93cb33c44c137234f53f8e360463525f5e5
DellsadaLvt <levantho0168@gmail.com>
2021-09-09 21:17:24 +0700

    Templete gpio

--- gpio/main/user_main.c
+++ gpio/main/user_main.c
@@ -40,7 +40,7 @@ static const char *TAG = "main";
  *
  */
 
-#define GPIO_OUTPUT_IO_0    15
+#define GPIO_OUTPUT_IO_0    2
 #define GPIO_OUTPUT_IO_1    16
 #define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0) | (1ULL<<GPIO_OUTPUT_IO_1))
 #define GPIO_INPUT_IO_0     4
